849096500f93fe1ed136d23c5acf325121153ade
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-30 17:18:32 +0530

    add WeatherManagerDelegate & LocationManagerDelegate

--- Clima/Controllers/ViewController.swift
+++ Clima/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     // IBOutlets
@@ -16,14 +17,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var cityLabel: UILabel!
     
     // Initialise
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     // Override View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Delegates
+        locationManager.delegate = self
+        weatherManager.delegate = self
+        searchTextField.delegate = self
+        // Authorisation
+        locationManager.requestWhenInUseAuthorization()
+        // Requestion Current Location
+        locationManager.requestLocation()
     }
     
     // IBActions
-    @IBAction func currentLocationPressed(_ sender: UIButton) {}
+    @IBAction func currentLocationPressed(_ sender: UIButton) {locationManager.requestLocation()}
     @IBAction func searchPressed(_ sender: UIButton) {}
 }
 
+//MARK:- LocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let currentLocation = locations.last?.coordinate {
+            weatherManager.fetchWeather(forLocation: currentLocation)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {print(error)}
+}
+
+//MARK:- WeatherManagerDelegate
+extension ViewController: WeatherManagerDelegate {
+    func didFetchWeather(_ weatherData: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weatherData.temperatureString
+            self.weatherImageView.image = UIImage(systemName: weatherData.condition)
+            self.cityLabel.text = weatherData.name
+        }
+    }
+    func didFailWithError(_ error: Error) {print(error)}
+}
+
+//MARK:- SearchTextFieldDelegate
+extension ViewController: UITextFieldDelegate {}
